dfe3e8706a32cc622fe5996a1bff6394ff6bd20c
Mihail Marian <mihailmarian12@gmail.com>
2020-12-05 09:36:15 +0200

    applied prettier to all files

--- src/index.js
+++ src/index.js
@@ -2,54 +2,67 @@ const BACKGROUNDS = [
   {
     min_temp: -99,
     max_temp: -5,
-    background_class: 'freezing-background'
+    background_class: "freezing-background",
   },
   {
     min_temp: -4,
     max_temp: 5,
-    background_class: 'cold-background'
+    background_class: "cold-background",
   },
   {
     min_temp: 6,
     max_temp: 16,
-    background_class: 'neutral-background'
+    background_class: "neutral-background",
   },
   {
     min_temp: 17,
     max_temp: 27,
-    background_class: 'warm-background'
+    background_class: "warm-background",
   },
   {
     min_temp: 28,
     max_temp: 99,
-    background_class: 'hot-background'
+    background_class: "hot-background",
   },
-]
+];
 
 const getWeather = async (location) => {
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7986a26ff47f7262c74542709f87e5e7&units=metric`;
 
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7986a26ff47f7262c74542709f87e5e7&units=metric`
-  
   let response = await fetch(url);
   let responseData = await response.json();
 
   let browserDate = new Date();
 
-  let dateLocal = new Date((responseData.dt+responseData.timezone+browserDate.getTimezoneOffset()*60)*1000);
+  let dateLocal = new Date(
+    (responseData.dt +
+      responseData.timezone +
+      browserDate.getTimezoneOffset() * 60) *
+      1000
+  );
 
-  let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-  let dateString = days[dateLocal.getDay()] + ', ' + dateLocal.toTimeString().slice(0,5);
+  let dateString =
+    days[dateLocal.getDay()] + ", " + dateLocal.toTimeString().slice(0, 5);
 
   return {
-    'city': responseData.name,
-    'country': responseData.sys.country,
-    'temperature': responseData.main.temp,
-    'description': responseData.weather[0].description,
-    'iconUrl': `http://openweathermap.org/img/wn/${responseData.weather[0].icon}@2x.png`,
-    'dateTime': dateString
+    city: responseData.name,
+    country: responseData.sys.country,
+    temperature: responseData.main.temp,
+    description: responseData.weather[0].description,
+    iconUrl: `http://openweathermap.org/img/wn/${responseData.weather[0].icon}@2x.png`,
+    dateTime: dateString,
   };
-}
+};
 
 const updateWeather = async (location) => {
   try {
@@ -57,54 +70,55 @@ const updateWeather = async (location) => {
 
     let temp = weatherData.temperature;
     let tempRound = Math.round(temp);
-    let newColors = BACKGROUNDS.filter(elem=>elem.max_temp>=tempRound && elem.min_temp <= tempRound)[0];
+    let newColors = BACKGROUNDS.filter(
+      (elem) => elem.max_temp >= tempRound && elem.min_temp <= tempRound
+    )[0];
 
     document.body.className = newColors.background_class;
 
-    document.getElementById('city-name').innerText = weatherData.city+', '+weatherData.country;
-    document.getElementById('date-time').innerText = weatherData.dateTime;
-    document.getElementById('description').innerText = weatherData.description.charAt(0).toUpperCase() + weatherData.description.slice(1);
-    document.getElementById('weather-icon').src = weatherData.iconUrl;
+    document.getElementById("city-name").innerText =
+      weatherData.city + ", " + weatherData.country;
+    document.getElementById("date-time").innerText = weatherData.dateTime;
+    document.getElementById("description").innerText =
+      weatherData.description.charAt(0).toUpperCase() +
+      weatherData.description.slice(1);
+    document.getElementById("weather-icon").src = weatherData.iconUrl;
 
-    let tempElem = document.getElementById('temperature');
-    tempElem.innerText = tempRound + '°';
-    tempElem.setAttribute('tempValue',temp);
-  }
-  catch (error) {
+    let tempElem = document.getElementById("temperature");
+    tempElem.innerText = tempRound + "°";
+    tempElem.setAttribute("tempValue", temp);
+  } catch (error) {
     console.log(`Error: ${error}`);
   }
-}
+};
 
-updateWeather('London');
+updateWeather("London");
 
-let button = document.querySelector('button');
+let button = document.querySelector("button");
 
-button.addEventListener('click',()=>{
-
-  button.classList.add('button-clicked');
+button.addEventListener("click", () => {
+  button.classList.add("button-clicked");
   setTimeout(() => {
-    button.classList.remove('button-clicked');
+    button.classList.remove("button-clicked");
   }, 200);
 
-  let input = document.querySelector('input').value;
+  let input = document.querySelector("input").value;
   updateWeather(input);
 });
 
-let aLinked = document.querySelector('a[href]');
+let aLinked = document.querySelector("a[href]");
 
-aLinked.addEventListener('click',(event)=>{
-
-  document.querySelector('a:not([href])').setAttribute('href','#');
-  event.target.removeAttribute('href');
+aLinked.addEventListener("click", (event) => {
+  document.querySelector("a:not([href])").setAttribute("href", "#");
+  event.target.removeAttribute("href");
 
   if (event.target.innerText == "F") {
+    let tempElem = document.getElementById("temperature");
 
-    let tempElem = document.getElementById('temperature');
-    
-    let celsTemp = parseFloat(tempElem.getAttribute('tempValue'));
-    
-    let fahrTemp = (celsTemp * 9/5) + 32;
-    tempElem.setAttribute('tempValue',fahrTemp);
+    let celsTemp = parseFloat(tempElem.getAttribute("tempValue"));
+
+    let fahrTemp = (celsTemp * 9) / 5 + 32;
+    tempElem.setAttribute("tempValue", fahrTemp);
     tempElem.innerText = Math.round(fahrTemp);
   }
-})
+});
