9cb3b053651927cded02417d732498641b809a94
Mihail Marian <mihailmarian12@gmail.com>
2020-12-06 12:11:09 +0200

    temp convert implemented

--- src/components/weather_table/weather_table.js
+++ src/components/weather_table/weather_table.js
@@ -1,6 +1,32 @@
 import "./weather_table.scss";
 import html from "./weather_table.html";
 
+const convertTemp = (clickedElem) => {
+  let tempElem = document.getElementById("temperature");
+  let originalTemp = parseFloat(tempElem.getAttribute("tempValue"));
+
+  let newTemp = 0;
+
+  if (clickedElem.innerText == "F") {
+    newTemp = (originalTemp * 9) / 5 + 32;
+  } else {
+    newTemp = ((originalTemp - 32) * 5) / 9;
+  }
+
+  tempElem.setAttribute("tempValue", newTemp);
+  tempElem.innerText = Math.round(newTemp) + "°";
+
+  let newConvertButton = document.querySelector("a:not([href])");
+  newConvertButton.setAttribute("href", "#");
+  newConvertButton.addEventListener("click", (event) => {
+    convertTemp(event.target);
+  });
+
+  let clickedElemReplacement = document.createElement("a");
+  clickedElemReplacement.innerText = clickedElem.innerText;
+  clickedElem.replaceWith(clickedElemReplacement);
+};
+
 const loadWeatherTable = (weatherData) => {
   let tableHtml = new DOMParser().parseFromString(html, "text/html");
 
@@ -17,6 +43,10 @@ const loadWeatherTable = (weatherData) => {
 
   tempElem.innerText = Math.round(tempValue) + "°";
   tempElem.setAttribute("tempValue", tempValue);
+
+  tableHtml.querySelector("a[href]").addEventListener("click", (event) => {
+    convertTemp(event.target);
+  });
 
   document
     .querySelector("main")
