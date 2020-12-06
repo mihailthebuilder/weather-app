03f2ea45c889c3876d8bdb95064d4364b9fdd871
Mihail Marian <mihailmarian12@gmail.com>
2020-12-06 13:11:40 +0200

    test for final build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { getWeather, updateBackground } from "./common/common.js";
 import { loadSearchBar } from "./components/search_bar/search_bar.js";
 import { loadWeatherTable } from "./components/weather_table/weather_table.js";
 
-// remember to apply try/catch
 const pageLoad = async (location) => {
   const weatherData = await getWeather(location);
 
@@ -13,23 +12,3 @@ const pageLoad = async (location) => {
 };
 
 pageLoad("London");
-
-/*
-
-let aLinked = document.querySelector("a[href]");
-
-aLinked.addEventListener("click", (event) => {
-  document.querySelector("a:not([href])").setAttribute("href", "#");
-  event.target.removeAttribute("href");
-
-  if (event.target.innerText == "F") {
-    let tempElem = document.getElementById("temperature");
-
-    let celsTemp = parseFloat(tempElem.getAttribute("tempValue"));
-
-    let fahrTemp = (celsTemp * 9) / 5 + 32;
-    tempElem.setAttribute("tempValue", fahrTemp);
-    tempElem.innerText = Math.round(fahrTemp);
-  }
-});
-*/
