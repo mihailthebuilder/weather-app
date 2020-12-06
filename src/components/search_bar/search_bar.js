0f5c6427c64acf9052d9b9cc117a685c16f1eec2
Mihail Marian <mihailmarian12@gmail.com>
2020-12-06 10:56:28 +0200

    search bar working

--- src/components/search_bar/search_bar.js
+++ src/components/search_bar/search_bar.js
@@ -5,13 +5,18 @@ import {
   loadWeatherTable,
   removeWeatherTable,
 } from "../weather_table/weather_table.js";
+import { updateBackground, getWeather } from "../../common/common.js";
 
 const loadSearchBar = async () => {
   document.querySelector("main").insertAdjacentHTML("afterbegin", html);
 
-  document.querySelector("form").addEventListener("submit", (event) => {
+  document.querySelector("form").addEventListener("submit", async (event) => {
     event.preventDefault();
     removeWeatherTable();
+
+    let weatherData = await getWeather(document.querySelector("input").value);
+    loadWeatherTable(weatherData);
+    updateBackground(weatherData.temperature);
   });
   //setTimeout needs to be made so that the query is put in the queue, thus waiting for the other async things to finish first (e.g. getWeatherData & updateBackground)
   setTimeout(() => {
